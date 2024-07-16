cac8aa1682648f4ef37338b68a1a1b5812b83148
Vladimir Efremov <vefremov@gmail.com>
2024-07-16 23:08:25 +0300

    Examples of using context.

--- ExampleNewZStep/main.py
+++ ExampleNewZStep/main.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 
 from di_lib import di_app
+from di_lib.di_app import Context
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
@@ -11,13 +12,14 @@ class ExampleNewZStep(di_app.DiAppSeismic3DMultiple):
     def __init__(self) -> None:
         super().__init__(in_name_par="seismic_3d",
                 out_name_par="result_name", out_names=["Example04-1"])
-        self.z_step_out = self.description["z_step"]
+        self.out_data_params["z_step"] = self.description["z_step"]
 
-    def compute(self, f_in_tup: Tuple[np.ndarray]) -> Tuple:
+    def compute(self, f_in_tup: Tuple[np.ndarray], context: Context) -> Tuple:
         # LOG.info(f"{self.cube_in} DT: {self.cube_in.time_step if self.cube_in else None}")
         f_in = f_in_tup[0]
         LOG.info(f"Computing {f_in.shape}")
-        new_nz = self.output_cubes_parameters["nz"]
+        LOG.info(f"Context: {context}")
+        new_nz = context.out_cube_params["nz"]
         new_shape = (f_in.shape[0], f_in.shape[1], new_nz)
         f_out = np.zeros(new_shape, dtype=f_in.dtype)
         return (f_out,)
