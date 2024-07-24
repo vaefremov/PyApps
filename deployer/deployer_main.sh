1a257f60f4b731a3bc91579c1b8e92f3607eca95
Vladimir Efremov <vefremov@gmail.com>
2024-07-24 22:02:38 +0300

    Pull and getting updates moved to main script.

--- deployer/deployer_main.sh
+++ deployer/deployer_main.sh
@@ -8,7 +8,21 @@ export DEPLOY_TO="/tmp/efremov/JobApps"
 export ATTIC="$DEPLOY_TO/../Attic"
 
 
-APPS=$($BIN/pull_check_updates.sh)
+pull_check_updates()
+{
+    cd $REPO
+
+    PREV_VER=$(git rev-parse HEAD)
+    git pull >/dev/null
+
+    FILES=$(git diff --name-only $PREV_VER origin/master)
+
+    APPS=$(for f in $FILES ;do echo $(dirname $f); done | sort | uniq)
+    echo $APPS
+}
+
+# APPS=$($BIN/pull_check_updates.sh)
+APPS=$(pull_check_updates)
 
 for a in $APPS
 do
