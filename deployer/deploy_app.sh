5cb52e2fae03cef46a636b2d4bdb47457c3a5474
Vladimir Efremov <vefremov@gmail.com>
2024-07-24 22:03:14 +0300

    Added app verification (to some extent).

--- deployer/deploy_app.sh
+++ deployer/deploy_app.sh
@@ -2,11 +2,17 @@
 
 APP=${1:?'App argument not set'}
 
-REPO=${REPO:?"/home/efremov/Projects/Repo1"}
-DEPLOY_TO=${DEPLOY_TO:?"/tmp/efremov/JobApps"}
+REPO=${REPO:?"No repository location specified"}
+DEPLOY_TO=${DEPLOY_TO:?"No deployment target"}
 ATTIC=$DEPLOY_TO/../Attic
 
 SUFFIX=$(date +"%Y%m%d%H%M%S")
+
+# Check if this is a correct app
+if [ !  -x $REPO/$APP/app.sh ];  then  echo "Error: app.sh not executable" 1>&2 && exit 1; fi
+if [ ! -f $REPO/$APP/config.json ]; then echo "Config missing" 1>&2 && exit 2; fi
+jsonlint-3 $REPO/$APP/config.json 1>&2
+if [ $? -ne 0 ]; then echo "Bad config" 1>&2 && exit 3; fi
 
 cd $DEPLOY_TO
 if [ -d $APP ]
