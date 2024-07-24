f8ca7768364d264969a2f78c61a30a8981e6039a
Vladimir Efremov <vefremov@gmail.com>
2024-07-24 22:44:37 +0300

    Script that calls main script periodically.

---
+++ deployer/polling_main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+# Script that runs deployer_main.sh periodically.
+
+BIN=$(readlink -f $(dirname $0))
+INTERVAL_SEC=10
+
+while true
+do
+  sleep $INTERVAL_SEC
+  $BIN/deployer_main.sh
+done
