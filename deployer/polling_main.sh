20b124309ad3fd2bc79dd28e13d5f6ebd9c16acb
Vladimir Efremov <vefremov@gmail.com>
2024-07-24 22:59:20 +0300

    Polling interval changed tp 30 s.

--- deployer/polling_main.sh
+++ deployer/polling_main.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 # Script that runs deployer_main.sh periodically.
 
 BIN=$(readlink -f $(dirname $0))
-INTERVAL_SEC=10
-
+INTERVAL_SEC=30
+echo "Start $INTERVAL_SEC sec " $(date) 
 while true
 do
   sleep $INTERVAL_SEC
