3a75b2ac1cc5393c6adc92303c0f463d61998e3a
Vladimir Efremov <vefremov@gmail.com>
2024-08-02 17:02:58 +0300

    Example03 test run documented.

--- Examples/Example03/runs.sh
+++ Examples/Example03/runs.sh
@@ -1,3 +1,1 @@
-./Example03/app.sh --job 19636672 --project 3344276 --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcl9uYW1lIjoiZWZyZW1vdiJ9.wRzAhnwnoDkxUmuZOz5mU9IlNBWicrF27a-Y8ULpA2Q -u http://localhost --port 9990
-# No 3d data:
-./Example03/app.sh --job 19636676 --project 3344276 --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcl9uYW1lIjoiZWZyZW1vdiJ9.wRzAhnwnoDkxUmuZOz5mU9IlNBWicrF27a-Y8ULpA2Q -u http://localhost --port 9990
+./Example03/app.sh --job 19778394 --project 19636343 --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcl9uYW1lIjoiZWZyZW1vdiJ9.wRzAhnwnoDkxUmuZOz5mU9IlNBWicrF27a-Y8ULpA2Q -u http://localhost --port 9990
