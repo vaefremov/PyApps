912bd2b1e83a90cfbc38dc921f95cb2fe93dfc8c
Rustavello <rustavello@mail.ru>
2024-08-20 19:57:44 +0300

    Обновление config

--- Butter/main.py
+++ Butter/main.py
@@ -15,7 +15,8 @@ MAXFLOAT = float(np.finfo(np.float32).max)
 
 class Butter (di_app.DiAppSeismic3D2D):
     def __init__(self) -> None:
-        super().__init__(in_name_par="Input Seismic3D Names", in_line_names_par="Input Seismic2D Names", 
+        super().__init__(in_name_par="Input Seismic3D Names", 
+                         in_line_geometries_par="Seismic2DGeometries", in_line_names_par="Input Seismic2D Names",
                 out_name_par="New Name", out_names=["Butter"])
         
         # Input datasets names are converted to the agreed upon format 
@@ -23,24 +24,24 @@ class Butter (di_app.DiAppSeismic3D2D):
         self.lowFreq = self.description["lowFreq"]
         self.step = self.description["step"] # input step is in ms, re-calculating to us
         self.kol_step = self.description["kol_step"]
-        #self.z_step = self.description["z_step"]
-        #self.out_data_params["z_step"] = self.step
+        self.z_step = self.description["z_step"]
+        self.out_data_params["z_step"] = self.step
        
     def compute(self, f_in_tup: Tuple[np.ndarray], context: Context) -> Tuple:
         tm_start = time.time()
         LOG.info(f"Computing {[f_in.shape for f_in in f_in_tup]}")
         LOG.info(f"Context: {context}")
         z_step = context.out_cube_params["z_step"]
-        #f_in= np.where((f_in_tup[0]>= 0.1*MAXFLOAT) | (f_in_tup[0]== np.inf), np.nan, f_in_tup[0])
+        f_in= np.where((f_in_tup[0]>= 0.1*MAXFLOAT) | (f_in_tup[0]== np.inf), np.nan, f_in_tup[0])
         fs = 1e6/z_step
         for i in range(self.lowFreq, self.lowFreq + self.kol_step + self.step + 2, self.step):
                 b, a = signal.butter(4, [i, i+1], fs=fs, btype='band')
                 f_out = signal.lfilter(b, a, f_in_tup[0])
                 print(f_out)
-        #np.nan_to_num(f_out, nan=MAXFLOAT, copy=False)
+        np.nan_to_num(f_out, nan=MAXFLOAT, copy=False)
         LOG.info(f"Processing time for fragment (s): {time.time() - tm_start}")
 
-        #return (f_out,)
+        return (f_out,)
 
 if __name__ == "__main__":
     LOG.debug(f"Starting job")
