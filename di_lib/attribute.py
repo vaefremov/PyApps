b0ffb19c948c8c6f247b70bf48b67387f3c98257
Vladimir Efremov <vefremov@gmail.com>
2024-09-05 14:35:19 +0200

    Horizon writer partially implemented.

--- di_lib/attribute.py
+++ di_lib/attribute.py
@@ -85,3 +85,44 @@ class DIHorizon3D:
             gr_arr = np.frombuffer(raw_data[8:], dtype=np.float32)
             gr_arr.shape = (nx, ny)
             return gr_arr
+class DIHorizon3DWriter(DIHorizon3D):
+    def __init__(self, project_id: int, name: str) -> None:
+        super().__init__(project_id, None, name)
+
+    def _init_from_info(self, i) -> None:
+        LOG.debug(f"{i}")
+        self.horizon_id = i["id"]
+        self.geometry_id = i["geometry_id"]
+        self.geometry_name = i["geometry_name"]
+        self.origin = i["origin"]
+        self.v_i = i["dx"]
+        self.v_x = i["dy"]
+        self.n_i = i["nx"]
+        self.n_x = i["ny"]
+        self.domain = i["domain"]
+        self.mode = i["mode"]
+
+    def _create(self) -> None:
+        #'http://localhost:8000/horizons/3d/create_empty/60/?geometry_id=19636981'
+        url = f"{self.server_url}/horizons/3d/create_empty/{self.project_id}/?geometry_id={self.geometry_id}"
+        res_status = 200
+        res_id = -1
+        try:
+            hor_out = self._get_info()
+            del hor_out["id"]
+            del hor_out["geometry_id"]
+            del hor_out["geometry_name"]
+            LOG.info(f"{hor_out=}")
+            body = json.dumps(hor_out).encode("utf8")
+            with requests.post(
+                    url, data=body, headers={"Content-Type": "application/json", "x-di-authorization": self.token}
+                ) as resp:
+                if resp.status_code != 200:
+                    LOG.error("Failed to create horizon, response code %s", resp.status_code)
+                    raise RuntimeError(f"Failed to create horizon, response code {resp.status_code}")
+                resp_j = json.loads(resp.content)
+                LOG.info("Reply: %s", resp_j)
+                self.cube_id = resp_j["id"]
+        except requests.exceptions.ConnectionError as ex:
+            LOG.error("Exception during POST: %s", str(ex))
+            raise ex
