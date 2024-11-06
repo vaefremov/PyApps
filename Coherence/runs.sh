17b33b4d4bdf5adba2008cfcf87a71301e6c01b0
Vladimir Efremov <vefremov@gmail.com>
2024-11-06 21:35:56 +0300

    Example runs.

---
+++ Coherence/runs.sh
@@ -0,0 +1,2 @@
+./JobApps/Coherence/app.sh  --job 19931012 --project 19638892 --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwidXNlcl9uYW1lIjoia2F6YW4ifQ.CeFkjtwL0JraAaLjRSHzLwKYRSV95oi2sH_Wrz5VUFI -u http://localhost --port 9990
+./JobApps/Coherence/app.sh  --job 19931018  --project 19637517 --token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwidXNlcl9uYW1lIjoia2F6YW4ifQ.CeFkjtwL0JraAaLjRSHzLwKYRSV95oi2sH_Wrz5VUFI -u http://localhost --port 9990 2>&1 | tee coherence.txt
