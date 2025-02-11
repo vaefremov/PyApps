a6682098a9f7bf1a06688383db80540865bc398a
garrifullin <garrifullini@gmail.com>
2025-02-11 21:42:20 +0300

    Исправление вычислении миксов при ненулевом shift

--- mixer/main.py
+++ mixer/main.py
@@ -20,33 +20,32 @@ def vec_corrcoef(X, Y, axis=1):
     d = np.std(Xm ,axis=axis)*np.std(Ym,axis=axis)
     return n / d
 
-def corelater(traces, shift, window, p, indC, idx, att_vec):
+def corelater(traces,shift,window,p,indC,idx,win_c,att_vec):
     x    = traces[indC[0],indC[1],:]
-    #Исключение точек координат, которых не существуют( выходят за границы куба)
+    
     pidx = np.where((p[:,0]>=0) & (p[:,1]>=0) & (p[:,0]<traces.shape[0]) & (p[:,1]<traces.shape[1]))
     p = p[pidx]
-    data = traces[p[:,0], p[:,1],:]
+    data = traces[p[:,0],p[:,1],:]
+    
     att_vec = att_vec[pidx]
-    # Исключение пустых соседних трасс
     not_nan_idx = ~np.isnan(data).all(axis=1)
     att_vec = att_vec[not_nan_idx]
     data = data[not_nan_idx]
-
     if shift == 0:
         mix = x + np.sum((data * att_vec[:,None]),axis=0)
     else:
         x_m    = np.lib.stride_tricks.sliding_window_view(x, axis=0, window_shape = 2 * window + 1)
         data_m = np.lib.stride_tricks.sliding_window_view(data, axis=1, window_shape = 2 * window + 1)
-        cor = np.zeros((2 * shift + 1,data.shape[0],x.shape[0] - 2 * shift - 2 * window))
-
+        cor = np.zeros((2 * shift + 1,data.shape[0],x.shape[0] - 2 * shift - 2 * window - 1 ))
+        
         for j in range(-shift, shift + 1):
-            cor[j,:,:] = vec_corrcoef(x_m[None,shift:x_m.shape[0] - shift,:], data_m[:,j + shift:data_m.shape[1]-shift + j,:], axis=2)
+            cor[j,:,:] = vec_corrcoef(x_m[None,shift:x_m.shape[0] - shift - 1,:], data_m[:,j + shift:data_m.shape[1] - shift + j - 1,:], axis=2)
         cor_idx = np.argmax(cor, axis=0)
-        all_idx = idx + cor_idx
+        all_idx = idx + cor_idx 
         # Выбор соответствующих сигналов из data
         result = data[np.arange(data.shape[0])[:, None], all_idx]
-        mix = x[window + shift: x.shape[0] - window-shift] + np.sum((result * att_vec[:,None]),axis=0)
-    return mix / (1 + np.sum(att_vec))
+        mix = x[win_c + shift: x.shape[0] - window-shift] + np.sum((result * att_vec[:,None]),axis=0)
+    return mix /(1.+np.sum(att_vec))
 
 def nokta(c, frm, halfwin_traces, type_neighbors):
     if frm == '3d':
@@ -72,19 +71,20 @@ def nokta(c, frm, halfwin_traces, type_neighbors):
             idx[2*m + 1] = c + m + 1
     return idx
 
-class Coherence(di_app.DiAppSeismic3D2D):
+class Mixer(di_app.DiAppSeismic3D2D):
     def __init__(self) -> None:
         super().__init__(in_name_par="Input Seismic3D Names", 
                          in_line_geometries_par="Seismic2DGeometries", in_line_names_par="Input Seismic2D Names",
-                out_name_par="New Name", out_names=["Coherence"])
+                out_name_par="New Name", out_names=["mix"])
         
         self.shift      = self.description["shift"]
         self.halfwin_traces = self.description["halfwin_traces"]
         self.attenuations   = self.description["attenuations"]
         self.type_neighbors = self.description["type"]
         self.window     = 5
+        self.win_c = self.window +1
         self.window = 0 if self.shift == 0 else  self.window
-
+        self.win_c = 0 if self.shift == 0 else  self.win_c
     def compute(self, f_in_tup: Tuple[np.ndarray], context: Context) -> Tuple:
         tm_start = time.time()
         if self.type_neighbors == "square":
@@ -98,8 +98,8 @@ class Coherence(di_app.DiAppSeismic3D2D):
                 att_vec[i * 4 : (i + 1) * 4] = self.attenuations **(i + 1) 
 
         f_in = copy.deepcopy(f_in_tup[0])
-
-        idx = np.arange(self.window+self.shift, f_in.shape[-1] - self.window-self.shift)
+        f_in = np.where((f_in>= 0.1*MAXFLOAT) | (f_in== np.inf), np.nan, f_in)
+        idx = np.arange(self.window+self.shift+1, f_in.shape[-1] - self.window-self.shift)
         if len(f_in.shape) == 3:
             frm = '3d'
             newTraces = np.full(f_in.shape, np.nan, dtype=np.float32)
@@ -110,8 +110,8 @@ class Coherence(di_app.DiAppSeismic3D2D):
                     else:
                         indC = [i, j]
                         indAll = nokta(indC, frm, self.halfwin_traces, self.type_neighbors)
-                        mix_sig = corelater(f_in, self.shift, self.window, indAll, indC, idx, att_vec)
-                        newTraces[indC[0],indC[1],self.window + self.shift:f_in.shape[2] - self.window - self.shift] = mix_sig
+                        mix_sig = corelater(f_in, self.shift, self.window, indAll, indC, idx,self.win_c, att_vec)
+                        newTraces[indC[0],indC[1], self.win_c + self.shift: f_in.shape[2] - self.window - self.shift] = mix_sig
         elif len(f_in.shape) == 2:
             frm = '2d'
             att_vec = np.zeros(self.halfwin_traces * 2)
@@ -125,8 +125,8 @@ class Coherence(di_app.DiAppSeismic3D2D):
                 else:
                     indC = i
                     indAll = nokta(indC, frm, self.halfwin_traces, self.type_neighbors)
-                    mix_sig = corelater(f_in, self.shift, self.window, indAll, indC, idx, att_vec)
-                    newTraces[indC,self.window + self.shift:f_in.shape[1] - self.window - self.shift] = mix_sig
+                    mix_sig = corelater(f_in, self.shift, self.window, indAll, indC, idx,self.win_c, att_vec)
+                    newTraces[indC[0],indC[1], self.win_c + self.shift: f_in.shape[2] - self.window - self.shift] = mix_sig
         else:
             raise ValueError(f"Unsupported input shape: {f_in.shape}")
         newTraces = newTraces.astype('float32')
@@ -136,6 +136,6 @@ class Coherence(di_app.DiAppSeismic3D2D):
 
 if __name__ == "__main__":
     LOG.debug(f"Starting job")
-    job = Coherence()
+    job = Mixer()
     res_final = job.run()
     LOG.info(f"{res_final}")
