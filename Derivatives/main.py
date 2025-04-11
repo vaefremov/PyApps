3599ad2766abec30d9ffbec085979f6d0bd70103
Islam Garifullin <garifullin1slam@yandex.ru>
2025-04-11 10:48:12 +0300

    Удаление параметра border_correction в модуле Derivative

--- Derivatives/main.py
+++ Derivatives/main.py
@@ -42,7 +42,7 @@ class Derivative(di_app.DiAppSeismic3D2D):
                     in_line_geometries_par="Seismic2DGeometries", in_line_names_par="Input Seismic2D Names",
                 out_name_par="New Name", out_names=[])
         
-        self.border_correction = self.description["border_correction"]
+        #self.border_correction = self.description["border_correction"]
         self.laplacian2d = self.description.get("Laplacian2d", True)
         self.first_derivative_Z = self.description.get("first_derivative_Z", True)
         out_names = ["Laplacian2d", "first derivative Z"]
